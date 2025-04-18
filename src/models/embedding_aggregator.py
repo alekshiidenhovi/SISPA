2aa1f609e03050435970cd369934239982463f02
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-04-18 02:41:08 +0000

    Update embedding aggregator docstring

--- src/models/embedding_aggregator.py
+++ src/models/embedding_aggregator.py
@@ -15,6 +15,17 @@ class EmbeddingAggregator(nn.Module):
         self.classifier = nn.Linear(hidden_dim, num_classes)
 
     def forward(self, embeddings: torch.Tensor):
+        """Forward pass of the embedding aggregator.
+
+        Args:
+            embeddings : torch.Tensor
+                Embeddings to aggregate. Shape is (N, embedding_dim * num_shards) where N is the number of samples in the batch, embedding_dim is the dimension of the embeddings produced by each shard, and num_shards is the number of shards.
+
+
+        Returns:
+            torch.Tensor
+                Logits for the aggregated embeddings. Shape is (N, num_classes) where N is the number of samples in the batch and num_classes is the number of classes.
+        """
         aggregated_embeddings = F.relu(self.aggregate(embeddings))
         logits = self.classifier(aggregated_embeddings)
         return logits
