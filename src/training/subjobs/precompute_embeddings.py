00aeea0fc99770f72bab6c0b64b657a5d0006d12
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-04-18 02:16:36 +0000

    Update precomputation interface to include the whole data

--- src/training/subjobs/precompute_embeddings.py
+++ src/training/subjobs/precompute_embeddings.py
@@ -8,7 +8,7 @@ from accelerate import Accelerator
 def precompute_embeddings(
     accelerator: Accelerator,
     prepared_model: torch.nn.Module,
-    prepared_dataloader: DataLoader,
+    prepared_train_dataloader: DataLoader,
     shard_idx: int,
 ):
     """
@@ -16,33 +16,34 @@ def precompute_embeddings(
 
     Parameters
     ----------
-    model : torch.nn.Module
+    accelerator : Accelerator
+        Accelerator to use for computing embeddings
+    prepared_model : torch.nn.Module
         Trained model to use for computing embeddings
-    dataloader : DataLoader
-        DataLoader for the dataset
-    device : torch.device
-        Device to compute embeddingss on
-    shard_idx : int, optional
-        Index of the shard if computing embeddings for a specific shard
+    prepared_train_dataloader : DataLoader
+        DataLoader for the entire training dataset
+    shard_idx : int
+        Index of the model shard being precomputed
 
     Returns
     -------
     tuple
-        Tuple of (embeddings, labels)
+        Tuple of (embeddings, labels). Embeddings are of shape (N, D) and labels are of shape (N,).
     """
     all_embeddings = torch.tensor([], device=accelerator.device, dtype=torch.float16)
     all_labels = torch.tensor([], device=accelerator.device, dtype=torch.int64)
-    prepared_model.eval()
 
-    dataloader_with_progress_bar = tqdm(prepared_dataloader)
+    dataloader_with_progress_bar = tqdm(
+        prepared_train_dataloader, desc=f"Precomputing embeddings for shard {shard_idx}"
+    )
     for batch_idx, (images, labels) in enumerate(dataloader_with_progress_bar):
         with accelerator.autocast():
-            embeddings = prepared_model(images)
-            all_embeddings = torch.cat((all_embeddings, embeddings), dim=0)
-            all_labels = torch.cat((all_labels, labels), dim=0)
-
             dataloader_with_progress_bar.set_description(
                 f"Precomputing embeddings for shard {shard_idx} - Batch {batch_idx}"
             )
+
+            embeddings = prepared_model(images)
+            all_embeddings = torch.cat((all_embeddings, embeddings), dim=0)
+            all_labels = torch.cat((all_labels, labels), dim=0)
 
     return all_embeddings, all_labels
