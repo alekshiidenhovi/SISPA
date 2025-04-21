b722e7384df98a84b6b1bc60b71f3fc5449ca782
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-04-21 17:16:38 +0000

    Update logging for training stage

--- src/training/subjobs/train_shard.py
+++ src/training/subjobs/train_shard.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from common.logger import logger
+import wandb
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from accelerate import Accelerator
@@ -19,6 +19,7 @@ def train_sharded_embedding_model(
     val_batch_interval: int,
     epochs: int,
     shard_idx: int,
+    wandb_run: wandb.wandb_run.Run,
 ) -> nn.Module:
     """
     Train a model on a specific shard of data.
@@ -42,6 +43,8 @@ def train_sharded_embedding_model(
             Index of the shard to train on
         epochs : int
             Number of epochs to train for
+        wandb_run : wandb.wandb_run.Run
+            W&B run to use for logging
 
     Returns:
         nn.Module
@@ -60,17 +63,13 @@ def train_sharded_embedding_model(
     prepared_embedding_model.train()
     prepared_classifier.train()
     for epoch_idx in range(epochs):
-        total_training_loss: float = 0.0
-        total_training_correct: int = 0
-        total_training_predicted: int = 0
-
-        progress_bar = tqdm(
-            prepared_train_dataloader,
-            desc=f"Training shard {shard_idx}, epoch {epoch_idx + 1}/{epochs}",
-        )
+        progress_bar = tqdm(prepared_train_dataloader)
         for batch_idx, (images, labels) in enumerate(progress_bar):
             with accelerator.accumulate(prepared_embedding_model):
                 with accelerator.autocast():
+                    progress_bar.set_description(
+                        f"Training shard {shard_idx}, epoch {epoch_idx + 1}/{epochs}, batch {batch_idx + 1}/{len(prepared_train_dataloader)}"
+                    )
                     embeddings = prepared_embedding_model(images)
                     outputs = prepared_classifier(embeddings)
                     loss, num_predicted, num_correct = compute_prediction_statistics(
@@ -78,23 +77,21 @@ def train_sharded_embedding_model(
                         outputs,
                         labels,
                     )
-                    total_training_loss += loss.item()
-                    total_training_predicted += num_predicted
-                    total_training_correct += num_correct
 
-                    progress_bar.set_postfix(
-                        {
-                            "training_loss": loss.item(),
-                            "training_accuracy": num_correct / num_predicted,
-                        }
-                    )
+                    training_metrics = {
+                        "training_loss": loss.item(),
+                        "training_accuracy": num_correct / num_predicted,
+                    }
+
+                    progress_bar.set_postfix(training_metrics)
+                    wandb_run.log(training_metrics)
 
                     accelerator.backward(loss)
                     prepared_optimizer.step()
                     prepared_optimizer.zero_grad()
 
             if (batch_idx + 1) % val_batch_interval == 0:
-                val_loss, val_accuracy = validate_shard_training(
+                validate_shard_training(
                     accelerator=accelerator,
                     prepared_embedding_model=prepared_embedding_model,
                     prepared_classifier=prepared_classifier,
@@ -105,20 +102,9 @@ def train_sharded_embedding_model(
                     batch_idx=batch_idx,
                     epochs=epochs,
                 )
-                logger.info(
-                    f"Shard {shard_idx}, epoch {epoch_idx + 1}, batch {batch_idx + 1}: "
-                    f"Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.2f}%"
-                )
 
                 prepared_embedding_model.train()
                 prepared_classifier.train()
-
-        final_accuracy = 100 * total_training_correct / total_training_predicted
-        logger.info(
-            f"Shard {shard_idx}, epoch {epoch_idx + 1}: "
-            f"Loss: {total_training_loss / len(prepared_train_dataloader):.4f}, "
-            f"Accuracy: {final_accuracy:.2f}%"
-        )
 
     return accelerator.unwrap_model(prepared_embedding_model).cpu()
 
@@ -134,6 +120,7 @@ def validate_shard_training(
     shard_idx: int,
     batch_idx: int,
     epochs: int,
+    wandb_run: wandb.wandb_run.Run,
 ):
     """
     Validate the model on a validation dataset.
@@ -158,6 +145,8 @@ def validate_shard_training(
         Index of the batch
     epochs : int
         Total number of epochs
+    wandb_run : wandb.wandb_run.Run
+        W&B run to use for logging
 
     Returns
     -------
@@ -170,13 +159,12 @@ def validate_shard_training(
     total_validation_correct: int = 0
     total_validation_predicted: int = 0
 
-    validation_progress_bar = tqdm(
-        prepared_val_dataloader,
-        desc=f"Validating shard {shard_idx}, epoch {epoch_idx + 1}/{epochs}, batch {batch_idx + 1}/{len(prepared_val_dataloader)}",
-    )
-
+    validation_progress_bar = tqdm(prepared_val_dataloader)
     for images, labels in validation_progress_bar:
         with accelerator.autocast():
+            validation_progress_bar.set_description(
+                f"Validating shard {shard_idx}, epoch {epoch_idx + 1}/{epochs}, batch {batch_idx + 1}/{len(prepared_val_dataloader)}"
+            )
             embeddings = prepared_embedding_model(images)
             outputs = prepared_classifier(embeddings)
             loss, num_predicted, num_correct = compute_prediction_statistics(
@@ -194,6 +182,12 @@ def validate_shard_training(
         if total_validation_predicted > 0
         else 0.0
     )
+
+    validation_metrics = {
+        "validation_loss": val_loss,
+        "validation_accuracy": val_accuracy,
+    }
+    wandb_run.log(validation_metrics)
 
     prepared_embedding_model.train()
     prepared_classifier.train()
