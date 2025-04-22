54412b41b26bd44810d7a324af3ef6a4b1f8d3c7
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-04-22 02:05:08 +0000

    Update naive retraining script

--- src/training/naive_retraining.py
+++ src/training/naive_retraining.py
@@ -2,10 +2,14 @@ import typing as T
 import torch
 import wandb
 import uuid
+import click
+import copy
 from accelerate import Accelerator
 from torch.utils.data import DataLoader, Subset, Dataset
-from torchvision import datasets
+from torchvision import datasets, transforms
 from prefect import flow, task
+from prefect.cache_policies import NO_CACHE
+import wandb.wandb_run
 from common.tracking import init_wandb_run
 from common.types import TrainingStep
 from models.resnet import ResNet
@@ -18,23 +22,22 @@ from training.subjobs.train_aggregation import train_aggregation_classifier
 from training.subjobs.train_shard import train_sharded_embedding_model
 from training.subjobs.create_dataset_splits import create_dataset_splits
 from training.subjobs.load_aggregation_dataloader import load_aggregation_dataloader
+from training.subjobs.collect_labels import collect_labels
 from storage.dataset_splits import SISPADatasetSplitsStorage
 from storage.embeddings import SISPAEmbeddingStorage
 from storage.models import SISPAModelStorage
 
 
-@task
+@task(cache_policy=NO_CACHE)
 def dataset_splits_task(
     dataset: Dataset,
     dataset_splits_storage: SISPADatasetSplitsStorage,
-    num_shards: int,
     train_val_test_split: T.Tuple[float, float, float],
     sampling_ratio: float,
     seed: int,
 ):
     train_shard_indices, val_indices, test_indices = create_dataset_splits(
         dataset=dataset,
-        num_shards=num_shards,
         train_val_test_split=train_val_test_split,
         sampling_ratio=sampling_ratio,
         seed=seed,
@@ -46,7 +49,7 @@ def dataset_splits_task(
     )
 
 
-@task
+@task(cache_policy=NO_CACHE)
 def load_shard_dataloaders_task(
     dataset: Dataset,
     dataset_splits_storage: SISPADatasetSplitsStorage,
@@ -56,7 +59,7 @@ def load_shard_dataloaders_task(
     num_shards: int,
 ):
     train_shard_indices, train_indices, val_indices, test_indices = (
-        dataset_splits_storage.retrieve_all_splits()
+        dataset_splits_storage.retrieve_all_splits_indices()
     )
     full_train_dataloader = DataLoader(
         Subset(dataset, train_indices),
@@ -94,7 +97,7 @@ def load_shard_dataloaders_task(
     )
 
 
-@task
+@task(cache_policy=NO_CACHE)
 def train_shards_task(
     accelerator: Accelerator,
     model_storage: SISPAModelStorage,
@@ -106,7 +109,7 @@ def train_shards_task(
     classifiers: T.List[torch.nn.Module],
     loss_fn: torch.nn.Module,
     epochs: int,
-    wandb_run: wandb.Run,
+    wandb_run: wandb.wandb_run.Run,
 ):
     for shard_idx, (
         untrained_embedding_model,
@@ -121,17 +124,32 @@ def train_shards_task(
             train_shard_dataloaders,
         )
     ):
+        (
+            prepared_embedding_model,
+            prepared_classifier,
+            prepared_optimizer,
+            prepared_train_dataloader,
+            prepared_val_dataloader,
+        ) = accelerator.prepare(
+            untrained_embedding_model,
+            classifier,
+            shard_optimizer,
+            train_shard_dataloader,
+            val_dataloader,
+        )
+
         trained_embedding_model = train_sharded_embedding_model(
             accelerator=accelerator,
-            embedding_model=untrained_embedding_model,
-            classifier=classifier,
-            optimizer=shard_optimizer,
-            train_dataloader=train_shard_dataloader,
-            val_dataloader=val_dataloader,
+            prepared_embedding_model=prepared_embedding_model,
+            prepared_classifier=prepared_classifier,
+            prepared_optimizer=prepared_optimizer,
+            prepared_train_dataloader=prepared_train_dataloader,
+            prepared_val_dataloader=prepared_val_dataloader,
             loss_fn=loss_fn,
             shard_idx=shard_idx,
             val_batch_interval=val_batch_interval,
             epochs=epochs,
+            wandb_run=wandb_run,
         )
 
         model_storage.save_sharded_model(
@@ -140,8 +158,22 @@ def train_shards_task(
             wandb_run=wandb_run,
         )
 
+        (
+            prepared_embedding_model,
+            prepared_classifier,
+            prepared_optimizer,
+            prepared_train_dataloader,
+            prepared_val_dataloader,
+        ) = accelerator.clear(
+            prepared_embedding_model,
+            prepared_classifier,
+            prepared_optimizer,
+            prepared_train_dataloader,
+            prepared_val_dataloader,
+        )
 
-@task
+
+@task(cache_policy=NO_CACHE)
 def precompute_embeddings_task(
     accelerator: Accelerator,
     embedding_model: torch.nn.Module,
@@ -162,6 +194,21 @@ def precompute_embeddings_task(
         trained_sispa_models
     )
 
+    all_training_labels = collect_labels(full_train_dataloader)
+    all_validation_labels = collect_labels(val_dataloader)
+
+    training_datapoint_ids = [str(uuid.uuid4()) for _ in all_training_labels]
+    validation_datapoint_ids = [str(uuid.uuid4()) for _ in all_validation_labels]
+
+    embedding_storage.initialize_datapoint_ids(
+        training_step=TrainingStep.TRAINING,
+        datapoint_ids=training_datapoint_ids,
+    )
+    embedding_storage.initialize_datapoint_ids(
+        training_step=TrainingStep.VALIDATION,
+        datapoint_ids=validation_datapoint_ids,
+    )
+
     for shard_idx, sispa_embedding_model in enumerate(
         trained_sispa_sharded_embedding_models
     ):
@@ -170,6 +217,7 @@ def precompute_embeddings_task(
             trained_model=sispa_embedding_model,
             dataloader=full_train_dataloader,
             shard_idx=shard_idx,
+            stage=TrainingStep.TRAINING,
         )
 
         validation_embeddings, validation_labels = precompute_embeddings(
@@ -177,51 +225,44 @@ def precompute_embeddings_task(
             trained_model=sispa_embedding_model,
             dataloader=val_dataloader,
             shard_idx=shard_idx,
+            stage=TrainingStep.VALIDATION,
         )
 
-        training_datapoint_ids = [str(uuid.uuid4()) for _ in training_labels]
-        validation_datapoint_ids = [str(uuid.uuid4()) for _ in validation_labels]
-
-        embedding_storage.store_embeddings(
+        embedding_storage.store_shard_embeddings(
             training_step=TrainingStep.TRAINING,
             shard_idx=shard_idx,
-            datapoint_ids=training_datapoint_ids,
             labels=training_labels,
             embeddings=training_embeddings,
         )
 
-        embedding_storage.store_embeddings(
+        embedding_storage.store_shard_embeddings(
             training_step=TrainingStep.VALIDATION,
             shard_idx=shard_idx,
-            datapoint_ids=validation_datapoint_ids,
             labels=validation_labels,
             embeddings=validation_embeddings,
         )
 
 
-@task
+@task(cache_policy=NO_CACHE)
 def load_aggregation_dataloaders_task(
     embedding_storage: SISPAEmbeddingStorage,
-    num_shards: int,
     train_batch_size: int,
     val_batch_size: int,
 ):
     training_embeddings_dataloader = load_aggregation_dataloader(
         embedding_storage=embedding_storage,
-        num_shards=num_shards,
         batch_size=train_batch_size,
         training_step=TrainingStep.TRAINING,
     )
     validation_embeddings_dataloader = load_aggregation_dataloader(
         embedding_storage=embedding_storage,
-        num_shards=num_shards,
         batch_size=val_batch_size,
         training_step=TrainingStep.VALIDATION,
     )
     return training_embeddings_dataloader, validation_embeddings_dataloader
 
 
-@task
+@task(cache_policy=NO_CACHE)
 def train_aggregator_task(
     accelerator: Accelerator,
     aggregator: SISPAEmbeddingAggregator,
@@ -232,14 +273,26 @@ def train_aggregator_task(
     val_batch_interval: int,
     epochs: int,
     model_storage: SISPAModelStorage,
-    wandb_run: wandb.Run,
+    wandb_run: wandb.wandb_run.Run,
 ):
+    (
+        prepared_aggregator,
+        prepared_aggregator_optimizer,
+        prepared_training_embeddings_dataloader,
+        prepared_validation_embeddings_dataloader,
+    ) = accelerator.prepare(
+        aggregator,
+        aggregator_optimizer,
+        training_embeddings_dataloader,
+        validation_embeddings_dataloader,
+    )
+
     trained_aggregator = train_aggregation_classifier(
         accelerator=accelerator,
-        aggregator=aggregator,
-        optimizer=aggregator_optimizer,
-        training_embeddings_dataloader=training_embeddings_dataloader,
-        validation_embeddings_dataloader=validation_embeddings_dataloader,
+        prepared_aggregator=prepared_aggregator,
+        prepared_optimizer=prepared_aggregator_optimizer,
+        prepared_training_embeddings_dataloader=prepared_training_embeddings_dataloader,
+        prepared_validation_embeddings_dataloader=prepared_validation_embeddings_dataloader,
         loss_fn=loss_fn,
         val_batch_interval=val_batch_interval,
         epochs=epochs,
@@ -250,7 +303,39 @@ def train_aggregator_task(
         wandb_run=wandb_run,
     )
 
+    (
+        prepared_aggregator,
+        prepared_aggregator_optimizer,
+        prepared_training_embeddings_dataloader,
+        prepared_validation_embeddings_dataloader,
+    ) = accelerator.clear(
+        prepared_aggregator,
+        prepared_aggregator_optimizer,
+        prepared_training_embeddings_dataloader,
+        prepared_validation_embeddings_dataloader,
+    )
 
+
+@click.command()
+@click.option("--epochs", type=int, default=1)
+@click.option("--num-shards", type=int, default=10)
+@click.option(
+    "--train-val-test-split",
+    type=click.Tuple([float, float, float]),
+    default=(0.8, 0.1, 0.1),
+)
+@click.option("--sampling-ratio", type=float, default=0.5)
+@click.option("--seed", type=int, default=42)
+@click.option("--train-batch-size", type=int, default=64)
+@click.option("--val-batch-size", type=int, default=64)
+@click.option("--test-batch-size", type=int, default=64)
+@click.option("--embedding-dim", type=int, default=64)
+@click.option("--hidden-dim", type=int, default=64)
+@click.option("--num-blocks", type=int, default=3)
+@click.option("--learning-rate", type=float, default=1e-4)
+@click.option("--weight-decay", type=float, default=1e-4)
+@click.option("--storage-path", type=str)
+@click.option("--val-batch-interval", type=int, default=50)
 @flow
 def naive_retraining(
     epochs: int,
@@ -278,13 +363,23 @@ def naive_retraining(
         storage_path=storage_path, embedding_dim=embedding_dim
     )
 
-    raw_dataset = datasets.MNIST(root="data", train=True, download=True)
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,)),
+        ]
+    )
+    raw_dataset = datasets.MNIST(
+        root="data", train=True, download=True, transform=transform
+    )
     num_classes = len(raw_dataset.targets.unique())
 
-    embedding_model = ResNet(num_blocks=num_blocks, hidden_dim=hidden_dim)
-    classifier = torch.nn.Linear(hidden_dim, num_classes)
-    untrained_embedding_models = [embedding_model.clone() for _ in range(num_shards)]
-    classifiers = [classifier.clone() for _ in range(num_shards)]
+    embedding_model = ResNet(num_blocks=num_blocks, embedding_dim=embedding_dim)
+    classifier = torch.nn.Linear(embedding_dim, num_classes)
+    untrained_embedding_models = [
+        copy.deepcopy(embedding_model) for _ in range(num_shards)
+    ]
+    classifiers = [copy.deepcopy(classifier) for _ in range(num_shards)]
     loss_fn = torch.nn.CrossEntropyLoss()
     shard_optimizers = [
         torch.optim.AdamW(
@@ -310,7 +405,6 @@ def naive_retraining(
     dataset_splits_task(
         dataset=raw_dataset,
         dataset_splits_storage=dataset_splits_storage,
-        num_shards=num_shards,
         train_val_test_split=train_val_test_split,
         sampling_ratio=sampling_ratio,
         seed=seed,
@@ -354,7 +448,6 @@ def naive_retraining(
     training_embeddings_dataloader, validation_embeddings_dataloader = (
         load_aggregation_dataloaders_task(
             embedding_storage=embedding_storage,
-            num_shards=num_shards,
             train_batch_size=train_batch_size,
             val_batch_size=val_batch_size,
         )
@@ -372,3 +465,7 @@ def naive_retraining(
         model_storage=model_storage,
         wandb_run=wandb_run,
     )
+
+
+if __name__ == "__main__":
+    naive_retraining()
