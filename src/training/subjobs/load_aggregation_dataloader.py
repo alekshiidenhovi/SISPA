a91caf92dcedd39401afaeb0c60281194a6d81c6
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-04-22 01:35:40 +0000

    Fix concatenation

--- src/training/subjobs/load_aggregation_dataloader.py
+++ src/training/subjobs/load_aggregation_dataloader.py
@@ -25,8 +25,14 @@ def load_aggregation_dataloader(
             logger.warning(f"No embeddings found for shard {shard_idx}")
             continue
 
-        all_embeddings = torch.cat((all_embeddings, torch.tensor(embeddings)), dim=1)
-        all_labels = torch.cat((all_labels, torch.tensor(labels)), dim=1)
+        if len(embeddings) == 0:
+            all_embeddings = torch.tensor(embeddings)
+            all_labels = torch.tensor(labels)
+        else:
+            all_embeddings = torch.cat(
+                (all_embeddings, torch.tensor(embeddings)), dim=1
+            )
+            all_labels = torch.cat((all_labels, torch.tensor(labels)), dim=1)
 
     embeddings_dataset = TensorDataset(all_embeddings, all_labels)
     embeddings_dataloader = DataLoader(
