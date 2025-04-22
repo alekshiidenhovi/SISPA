bb50ea4305cfe9c9b70fc087c7ae3223ff472361
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-04-22 19:41:36 +0000

    Update training config

--- src/common/config.py
+++ src/common/config.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, Field, field_validator
 import typing as T
 import multiprocessing
-from accelerate.utils.dataclasses import PrecisionType
 from common.tracking import init_wandb_api_client
+from datasets.choose_dataset_split_strategy import DatasetSplitStrategy
 
 ACCELERATOR = T.Literal["gpu", "cpu", "tpu"]
 
@@ -14,21 +14,18 @@ class DatasetConfig(BaseModel):
     dataset splits, and sampling configurations for training, validation and testing.
     """
 
-    dataset_dir: str = Field(
-        description="Path to the dataset directory",
-    )
     train_batch_size: int = Field(
-        default=32,
+        default=64,
         ge=1,
         description="Batch size for training",
     )
     val_batch_size: int = Field(
-        default=32,
+        default=64,
         ge=1,
         description="Batch size for validation",
     )
     test_batch_size: int = Field(
-        default=32,
+        default=64,
         ge=1,
         description="Batch size for testing",
     )
@@ -37,13 +34,8 @@ class DatasetConfig(BaseModel):
         description="Number of workers for data loading",
         ge=1,
     )
-    img_size: int = Field(
-        default=28,
-        ge=32,
-        description="Size of the input images",
-    )
     train_val_test_split: T.Tuple[float, float, float] = Field(
-        default=(0.95, 0.025, 0.025),
+        default=(0.8, 0.1, 0.1),
         description="Train, validation and test split proportions",
     )
     num_shards: int = Field(
@@ -51,15 +43,15 @@ class DatasetConfig(BaseModel):
         ge=1,
         description="Number of shards for SISA training",
     )
-    target_class_ratio: float = Field(
+    class_informed_strategy_sampling_ratio: float = Field(
         default=0.5,
         ge=0.0,
         le=1.0,
         description="Proportion of samples from target class in each shard",
     )
-    embeddings_dir: str = Field(
-        default="./.embeddings/",
-        description="Directory to save precomputed embeddings",
+    dataset_split_strategy: str = Field(
+        default=DatasetSplitStrategy.EQUAL.value,
+        description="Strategy to use for dataset splitting",
     )
 
     @field_validator("train_val_test_split")
@@ -75,11 +67,14 @@ class DatasetConfig(BaseModel):
 class ModelConfig(BaseModel):
     """Configuration for the model architecture."""
 
-    hidden_dim: int = Field(
-        default=128, ge=1, description="Hidden dimension of the model"
+    backbone_embedding_dim: int = Field(
+        default=64, ge=1, description="Embedding dimension of the backbone model"
     )
-    num_blocks: int = Field(
-        default=3, ge=1, description="Number of blocks in the model"
+    resnet_num_blocks: int = Field(
+        default=3, ge=1, description="Number of blocks in the Resnet backbone model"
+    )
+    aggregator_hidden_dim: int = Field(
+        default=128, ge=1, description="Hidden dimension of the aggregation model"
     )
 
 
@@ -94,32 +89,16 @@ class FinetuningConfig(BaseModel):
         default="gpu",
         description="Compute accelerator to use for training",
     )
-    enable_checkpointing: bool = Field(
-        default=True, description="Enables checkpointing of the latest training epoch"
+    val_check_interval_percentage: float = Field(
+        default=50,
+        ge=0.0,
+        le=1.0,
+        description="The percentage of training batches of an epoch after which to run validation",
     )
-    val_check_interval: int = Field(
-        default=200,
-        ge=1,
-        description="The interval of training batched to run validation",
-    )
-    limit_val_batches: int = Field(
-        default=0.1, description="Number of validation batches to run", ge=1
-    )
-    precision: str = Field(
-        default=PrecisionType.BF16.value,
-        description="Precision of gradients and the model used during finetuning",
-    )
-    model_checkpoint_path: str = Field(
-        default="./.checkpoints/",
-        description="Folder for saving model checkpoints during training",
-    )
-    final_model_path: str = Field(
-        default="./.checkpoints/final_model/",
-    )
-    max_epochs: int = Field(
+    epochs: int = Field(
         default=8,
         ge=1,
-        description="Maximum number of epochs to train for",
+        description="Number of epochs to train for",
     )
     devices: T.List[int] = Field(
         default=[0],
@@ -130,10 +109,6 @@ class FinetuningConfig(BaseModel):
         ge=1,
         description="Number of batches to accumulate gradients before updating the model",
     )
-    gradient_checkpointing: bool = Field(
-        default=False,
-        description="Enables gradient checkpointing for memory efficient training",
-    )
 
 
 class OptimizerConfig(BaseModel):
@@ -142,24 +117,24 @@ class OptimizerConfig(BaseModel):
     Contains parameters for the optimizer including weight decay, beta1 and beta2 parameters for the Adam optimizer.
     """
 
-    weight_decay: float = Field(
+    optimizer_weight_decay: float = Field(
         default=0.01,
         ge=0,
         description="Weight decay for the optimizer",
     )
-    adam_beta1: float = Field(
+    optimizer_adam_beta1: float = Field(
         default=0.9,
         ge=0.0,
         le=1.0,
         description="Beta1 parameter for the Adam optimizer, used for the first moment estimate",
     )
-    adam_beta2: float = Field(
+    optimizer_adam_beta2: float = Field(
         default=0.999,
         ge=0.0,
         le=1.0,
         description="Beta2 parameter for the Adam optimizer, used for the second moment estimate",
     )
-    learning_rate: float = Field(
+    optimizer_learning_rate: float = Field(
         default=1e-5, ge=0, description="Learning rate of the model"
     )
 
@@ -173,6 +148,9 @@ class TrainingConfig(DatasetConfig, ModelConfig, FinetuningConfig, OptimizerConf
     seed: int = Field(
         default=42,
         description="Seed for training reproducibility",
+    )
+    storage_path: str = Field(
+        description="Directory to save training artifacts, such as dataset splits, precomputed embeddings, and trained model weights",
     )
 
     def get_dataset_config(self) -> DatasetConfig:
